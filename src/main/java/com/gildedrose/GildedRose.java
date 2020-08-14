133e7aa5fe8e397f570b24061fe031c5d627cf11
Gaëtan Puget <gpuget@excilys.com>
2020-08-14 16:50:08 +0200

    Refacto all class

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,15 +1,22 @@
 package com.gildedrose;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.IntConsumer;
 
 class GildedRose {
 	private static final int QUALITY_MIN = 0;
 	private static final int QUALITY_MAX = 50;
 	private static final int QUALITY_STEP = 1;
-	private static final int BACKSTAGE_FIRST_LIMIT = 11;
-	private static final int BACKSTAGE_SECOND_LIMIT = 6;
 
+	private static final int SELL_IN_LIMIT = 0;
+	private static final int SELL_IN_STEP = 1;
+
+	private static final int BACKSTAGE_FIRST_LIMIT = 10;
+	private static final int BACKSTAGE_SECOND_LIMIT = 5;
+	private static final int BACKSTAGE_LAST_LIMIT = SELL_IN_LIMIT;
 	private static final String BACKSTAGE_ITEM = "Backstage passes to a TAFKAL80ETC concert";
+
 	private static final List<String> ITEMS_GAIN_QUALITY_WITH_AGE = List.of("Aged Brie", BACKSTAGE_ITEM);
 	private static final List<String> LEGENDARY_ITEMS = List.of("Sulfuras, Hand of Ragnaros");
 
@@ -20,63 +27,53 @@ class GildedRose {
 	}
 
 	public void updateQuality() {
-		for (Item item : items) {
-			if (!LEGENDARY_ITEMS.contains(item.name)) {
-				affectQuality(item);
-				affectSellIn(item);
-			}
-		}
+		Arrays.stream(this.items).filter(item -> !LEGENDARY_ITEMS.contains(item.name)).forEach(this::updateQuality);
+	}
+
+	private void updateQuality(Item item) {
+		affectSellIn(item);
+		affectQuality(item);
+	}
+
+	private void affectSellIn(Item item) {
+		item.sellIn = item.sellIn - SELL_IN_STEP;
 	}
 
 	private void affectQuality(Item item) {
+		int outdatedDegradationFactor = outdatedDegradationFactor(item);
 		if (ITEMS_GAIN_QUALITY_WITH_AGE.contains(item.name)) {
-			increaseQuality(item);
+			int qualityFactor = qualityFactor(item);
+			increaseQuality(item, outdatedDegradationFactor * qualityFactor);
 		} else {
-			decreaseQuality(item);
+			decreaseQuality(item, outdatedDegradationFactor);
 		}
 	}
 
-	private void decreaseQuality(Item item) {
-		item.quality = Math.max(item.quality - QUALITY_STEP, QUALITY_MIN);
+	private int outdatedDegradationFactor(Item item) {
+		return (item.sellIn < SELL_IN_LIMIT) ? 2 : 1;
 	}
 
-	private void increaseQuality(Item item) {
-		int modifier = 1;
-		if (BACKSTAGE_ITEM.equals(item.name)) {
-			modifier = backstageQualityModifier(item);
-		}
-
-		item.quality = Math.min(item.quality + (modifier * QUALITY_STEP), QUALITY_MAX);
+	private void increaseQuality(Item item, int factor) {
+		item.quality = Math.min(item.quality + (factor * QUALITY_STEP), QUALITY_MAX);
 	}
 
-	private int backstageQualityModifier(Item item) {
-		if (item.sellIn < BACKSTAGE_SECOND_LIMIT) {
+	private void decreaseQuality(Item item, int factor) {
+		item.quality = Math.max(item.quality - (factor * QUALITY_STEP), QUALITY_MIN);
+	}
+
+	private int qualityFactor(Item item) {
+		return (BACKSTAGE_ITEM.equals(item.name)) ? backstageQualityFactor(item) : 1;
+	}
+
+	private int backstageQualityFactor(Item item) {
+		if (item.sellIn < BACKSTAGE_LAST_LIMIT) {
+			return 0;
+		} else if (item.sellIn < BACKSTAGE_SECOND_LIMIT) {
 			return 3;
 		} else if (item.sellIn < BACKSTAGE_FIRST_LIMIT) {
 			return 2;
 		} else {
 			return 1;
-		}
-	}
-
-	private void affectSellIn(Item item) {
-		item.sellIn = item.sellIn - 1;
-		if (item.sellIn < 0) {
-			if (!item.name.equals("Aged Brie")) {
-				if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-					if (item.quality > 0) {
-						if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-							item.quality = item.quality - 1;
-						}
-					}
-				} else {
-					item.quality = item.quality - item.quality;
-				}
-			} else {
-				if (item.quality < 50) {
-					item.quality = item.quality + 1;
-				}
-			}
 		}
 	}
 }
